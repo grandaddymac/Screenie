39900de311ff3bec5cbbe8aa54a935147d9db826
Charles Davis <tgcd5@me.com>
2019-01-09 17:39:07 -0500

    Initial Commit

---
+++ Screenie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Screenie
+//
+//  Created by gdm on 1/9/19.
+//  Copyright © 2019 gdm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
